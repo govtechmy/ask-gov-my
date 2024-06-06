0da04f8d41d5d6948095bfb59aaf4ddfe9021268
Wan Aqim <wanaqim@Wans-MacBook-Pro.local>
2024-06-06 20:17:36 +0800

    add AJAX function mockup to plane.so

--- frontend/src/app/API Services/index.ts
+++ frontend/src/app/API Services/index.ts
@@ -1,4 +1,5 @@
 'use server'
+import { NextApiRequest, NextApiResponse } from "next";
 // this file will act as a middlepart for the components and API / mockup db in next
 
 // todo list: 
@@ -28,3 +29,48 @@ export async function submitQuestion(title: string, description: string) {
     data: { title, description },
   });
 }
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const slug = process.env.NEXT_PUBLIC_SLUG;
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+const headers = {
+  'X-API-Key': apiKey,
+}
+
+// export async function fetchQuestions() {
+//   const url = `https://api.plane.so/api/v1/workspaces/${slug}/projects/${projectId}/issues/`;
+//   const response = await fetch(url, { headers });
+
+//   if (!response.ok) {
+//     throw new Error(`Error: ${response.statusText}`);
+//   }
+
+//   const data = await response.json();
+//   return data;
+// }
+
+// export async function submitQuestion(title: string, description: string) {
+//   const slug = process.env.NEXT_PUBLIC_SLUG;
+//   const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
+//   const url = `https://api.plane.so/api/v1/workspaces/${slug}/projects/${projectId}/issues/`;
+  
+//   const body = JSON.stringify({
+//     title,
+//     description,
+//   });
+  
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body,
+//   });
+
+//   if (!response.ok) {
+//     throw new Error(`${response.statusText}`);
+//   }
+//   const data = await response.json();
+  
+//   return data
+// }
