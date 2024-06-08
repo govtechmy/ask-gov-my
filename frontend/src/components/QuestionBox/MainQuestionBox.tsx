682e5cdbf365c2349b45d619b509f0e507c6354d
Wan Aqim <wanaqim@gmail.com>
2024-06-09 07:31:12 +0800

    import getRandomQuestion and console.log to test

--- frontend/src/components/QuestionBox/MainQuestionBox.tsx
+++ frontend/src/components/QuestionBox/MainQuestionBox.tsx
@@ -1,10 +1,38 @@
+'use client'
+import { useEffect, useState } from "react";
 import OverflowContainerQuestBox from "./OverflowContainerQuestBox";
+import { getRandomQuestionsFromDB } from "@/API Services/questionServices";
 
+interface Question {
+    id: string;
+    agency: string;
+    description: string;
+    name: string;
+  }
+  
 const MainQuestionBox = () => {
+    const [questions, setQuestions] = useState<Question[]>([]);
+
+    const fetchQuestions = async () => {
+        try {
+            const fetchedQuestions: Question[] = await getRandomQuestionsFromDB();
+            setQuestions(fetchedQuestions);
+        } catch (error) {
+            console.error("Error fetching questions: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchQuestions();
+    }, []);
+
+    console.log(questions);
+
+
     return (
         <header className="p-5 bg-#0000ff text-left border border-black rounded-md">
             <h1>Top Questions From Citizens</h1>
-            {/* ubah some detail question box so it can onlyt show limit of the box */}
+            {/* change some detail question box so it can onlyt show limit of the box */}
             <OverflowContainerQuestBox></OverflowContainerQuestBox>
             <OverflowContainerQuestBox></OverflowContainerQuestBox>
             <OverflowContainerQuestBox></OverflowContainerQuestBox>
