68033d5f66034cf55dda4f82769888c9e17474d0
harrisazmi <azmi.harris@gmail.com>
2024-06-08 11:57:33 +0800

    Updated docker-compose and readmedocker

--- frontend/backup.sh
+++ frontend/backup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-set -e
+# Restore the database from db_backup.sql
+psql -h postgres -U $POSTGRES_USER -d $POSTGRES_DB -f /backups/db_backup.sql
 
-# Wait for the PostgreSQL container to be ready
-until pg_isready -h postgres -U $POSTGRES_USER; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 2
-done
-
-# Dump the database
-pg_dumpall -h postgres -U $POSTGRES_USER -f /backups/db_backup.sql
-echo "Database backup completed."
+# You can add other backup commands here
