a42098cc41237c7e76c1ede06c5a1aa0d1da48e9
harrisazmi <azmi.harris@gmail.com>
2024-06-09 22:05:28 +0800

    Edited simple ones

--- frontend/src/components/ClickHomeLogo.tsx
+++ frontend/src/components/ClickHomeLogo.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 const ClickHomeLogo = () => {
     return (
-        <header className="p-5 bg-#0000ff text-left border border-black rounded-md">
+        <div className="p-5 bg-#0000ff text-left border border-black rounded-md">
             <div>
                 <Link href="/">
                         <h1 style={{ cursor: 'pointer' }}>ask.gov.my click home logo</h1>
                 </Link>
             </div>
-        </header>
+        </div>
     );
 };
 
