71ae3db8c1718b4de7606303fc742bad970d472c
harrisazmi <azmi.harris@gmail.com>
2024-06-13 15:19:59 +0800

    header ready

--- frontend/src/components/ClickHomeLogo.tsx
+++ frontend/src/components/ClickHomeLogo.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const ClickHomeLogo = () => {
     return (
-        <div className="p-5 bg-#0000ff text-left border border-black rounded-md">
+        <div className="p-5 text-left">
             <div>
-                <Link href="/MainPage">
-                        <h1 style={{ cursor: 'pointer' }}>ask.gov.my click home logo</h1>
+                <Link href="/">
+                        <h1 style={{ cursor: 'pointer' }}>
+                            <Image                             
+                            src="/askgovmy-logo.svg"
+                            width={150}
+                            height={150}
+                            alt="ask logo">
+                            </Image>
+                        </h1>
                 </Link>
             </div>
         </div>
