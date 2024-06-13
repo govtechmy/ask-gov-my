1c57a83506c926e0a248601ab3f42c1bf0a9d18f
harrisazmi <azmi.harris@gmail.com>
2024-06-13 12:55:25 +0800

    updated logo from sanbron but color seems diff

--- frontend/src/components/FooterDetails/Footer.tsx
+++ frontend/src/components/FooterDetails/Footer.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <div className="bg-background dark:bg-white border-t pt-12 pb-16 z-10">
-            <div className="container mx-auto px-6 lg:px-16 flex flex-col lg:flex-row justify-between max-w-screen-2xl">
-                <div className="flex flex-col lg:flex-row items-center lg:items-start mb-6 lg:mb-0">
+            <div className="flex flex-col lg:flex-row">
+                <div className="flex flex-col sm:px-6 lg:flex-row items-start lg:items-start mb-6 lg:mb-0">
                     <div className="flex items-center">
                         <Image
                             src="/jata_logo.png"
@@ -19,8 +19,8 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col lg:flex-row lg:w-3/4 justify-end">
-                    <div className="mb-6 lg:mb-0 lg:pr-12">
+                <div className="flex flex-col lg:flex-row lg:w-3/4 justify-end sm:pl-6">
+                    <div className="mb-6 lg:mb-0 lg:pr-12 ">
                         <div className="font-bold">Open Source</div>
                         <p className="hover:underline block mt-2">
                             <Link href="https://www.google.com">
