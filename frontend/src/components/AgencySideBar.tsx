8341914bc90a3dad7a65f97f4d684984bea51e96
Wan Aqim <wanaqim@gmail.com>
2024-06-15 17:53:05 +0800

    add search/filter functionality by typing acronym / agency name

--- frontend/src/components/AgencySideBar.tsx
+++ frontend/src/components/AgencySideBar.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import React from 'react';
 
 interface Agency {
     id: string;
@@ -12,8 +12,8 @@ interface AgencySidebarProps {
 }
 
 const AgencySidebar: React.FC<AgencySidebarProps> = ({ agencies }) => {
-
     const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState('');
 
     const handleAgencyClick = (id: string) => {
         router.push(`/${id}`);
@@ -33,11 +33,26 @@ const AgencySidebar: React.FC<AgencySidebarProps> = ({ agencies }) => {
         return { formattedName, acronym };
     };
 
+    const filteredAgencies = agencies.filter(agency => {
+        const { formattedName, acronym } = formatAgencyName(agency.name);
+        return (
+            formattedName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            acronym.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    });
+
     return (
         <div className="px-4 py-4 lg:px-10 max-w-screen-lg mx-auto">
-            <div className='py-2 pb-4 font-semibold'>Agency</div>
+            <div className="py-2 pb-4 font-semibold">Agency</div>
+            <input
+                type="text"
+                placeholder="Search agencies..."
+                className="w-full p-2 mb-4 border border-gray-300 rounded-md"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <ul className="space-y-2">
-                {agencies.map(agency => {
+                {filteredAgencies.map(agency => {
                     const { formattedName, acronym } = formatAgencyName(agency.name);
                     return (
                         <li 
@@ -46,7 +61,7 @@ const AgencySidebar: React.FC<AgencySidebarProps> = ({ agencies }) => {
                             onClick={() => handleAgencyClick(agency.id)}
                         >
                             <div className="flex justify-between items-center">
-                                <span className=" truncate text-gray-500">{formattedName}</span>
+                                <span className="truncate text-gray-500">{formattedName}</span>
                                 <span className="ml-2 text-sm text-gray-600 truncate">{acronym}</span>
                             </div>
                         </li>
