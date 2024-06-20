9cca318b34d2287941bd391c09bdd3267b9d854c
Juwaini <juwaini@gmail.com>
2024-06-20 09:09:40 +0800

    link ministry and questions with answer (WIP)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,13 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from plane.views import MinistryView, QnAView
+from plane.views import MinistryView, QuestionsView, QnAView, AnswerView
 from search.views import SearchView, SearchAgenciesView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MinistryView.as_view(), name='ministry'),
-    path('qna/<uuid:id>', QnAView.as_view(), name='qna'),
+    path('questions', QuestionsView.as_view(), name='questions'),
+    path('answer/<uuid:agency_id>/<uuid:question_id>', AnswerView.as_view(), name='answer'),
+    path('question/<uuid:id>', QnAView.as_view(), name='question'),
     path('search', SearchView.as_view(), name='search'),
     path('agency-search', SearchAgenciesView.as_view(), name='agency_search'),
 ]
--- backend/plane/views.py
+++ backend/plane/views.py
@@ -14,10 +14,29 @@ class MinistryView(TemplateView):
         return context
 
 
+class QuestionsView(TemplateView):
+    template_name = 'questions.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['questions'] = plane.get_all_questions()
+        return context
+
+
 class QnAView(TemplateView):
     template_name = 'qna.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['qna'] = plane.get_question_by_agency(kwargs['id'])
+        return context
+
+
+class AnswerView(TemplateView):
+    template_name = 'answer.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['question'] = plane.get_question_by_agency(kwargs['agency_id'])
+        context['answer'] = plane.get_answer(kwargs['agency_id'], kwargs['question_id'])
         return context
