52e076d4b85416b90a5e1de66b43f1af584de333
Wan Aqim <wanaqim@gmail.com>
2024-06-25 10:56:11 +0800

    add new endpoint

--- backend/ask_gov/urls.py
+++ backend/ask_gov/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import QuestionListCreateView, QuestionDetailView, AgencyListView, submit_question
+from .views import QuestionListCreateView, QuestionDetailView, AgencyListView, submit_question, get_questions_by_agency
 
 urlpatterns = [
     path('questions/', QuestionListCreateView.as_view(), name='question-list-create'),
     path('questions/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
     path('agencies/', AgencyListView.as_view(), name='agency-list'),
     path('submit-question/<int:agency_id>/', submit_question, name='submit-question'),
+    path('questions-by-agency/<int:agency_id>/', get_questions_by_agency, name='questions-by-agency'),
 ]
