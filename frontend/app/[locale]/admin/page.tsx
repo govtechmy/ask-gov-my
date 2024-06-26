7d5cc63b779d3967bb132e851a00928ea081b49e
harrisazmi <azmi.harris@gmail.com>
2024-06-26 16:31:52 +0800

    finish login admin

--- frontend/app/[locale]/admin/page.tsx
+++ frontend/app/[locale]/admin/page.tsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
-import Header from "@/components/HeaderDetails/Header";
+import HeaderAdmin from "@/components/HeaderDetails/HeaderAdmin";
 import FooterAdmin from "@/components/FooterDetails/FooterAdmin";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import Google from "@/icons/google";
 
 export function AdminPage() {
   return (
     <div className="flex flex-col min-h-screen"> 
-      <Header />
-      <div className="flex-grow flex items-center justify-center py-12"> 
-        <div className="mx-auto">
+      <HeaderAdmin />
+      <div className="flex-grow flex items-center justify-center py-12 border">
+        <div className=" min-w-96 ">
           <div className="text-center">
-            <div className="text-2xl font-semibold">Login to AskGovMy</div>
-            <p className="text-balance text-muted-foreground">
-              Welcome back! Please enter your details.
-            </p>
+            <div className="text-2xl font-semibold px-5 py-5">Log in to AskGovMy</div>
+            <div className="text-base pb-6 text-zinc-700">Welcome back! Please enter your details.</div>
           </div>
 
           <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+
+            <div className="grid gap-2 pb-4">
+              <div className="text-zinc-500 text-sm">Email</div>
               <Input
                 id="email"
                 type="email"
@@ -28,23 +27,23 @@ export function AdminPage() {
                 required
               />
             </div>
-
-            {/* Removed empty grid gap-2 div */}
             
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="text-base font-medium text-white rounded-md py-2 from-[#702FF9] to-[#B379FF]  dark:from-[#702FF9] dark:to-[#B379FF] border-[1px] border-[#702FF9]">
               Continue with Email
             </Button>
             
-            <div className="text-center">OR</div>
-            
-            <Button className="w-full">
-              Continue with MyGovUC
+            <div className="text-center font-normal text-zinc-500 text-sm">OR</div>
+
+            <Button className="flex justify-center py-2 rounded-md from-[#FFF] to-[#FFF]  dark:from-[#18181B] dark:to-[#18181B] border-[1px] border-[#E4E4E7]">
+                <Google></Google>
+                <div className="px-2 font-medium text-base  ">Continue with MyGovUC</div>
             </Button>
+
           </div>
 
-          <div className="mt-4 text-center text-sm">
-            <Link href="/forgot-password" className="ml-auto inline-block text-sm">
-              Forgot your password?
+          <div className="mt-4 text-center pt-4">
+            <Link href="/forgot-password" className="ml-auto font-normal text-[#2563EB] inline-block text-sm">
+              Forgot password?
             </Link>
           </div>
         </div>
--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -4,19 +4,19 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, forwardRef } from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm text-start font-medium select-none focus-visible:outline-primary disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed",
+  "",
   {
     variants: {
       variant: {
         default:
-          "from-blue-600 to-[#3E7AFF] shadow-sm bg-gradient-to-t text-white hover:to-[#5B8EFF]",
+          "bg-gradient-to-t ",
         secondary:
-          "border border-outline-200 hover:border-outline-300 bg-background text-foreground",
+          "",
         tertiary: "",
       },
       size: {
-        default: "gap-1.5 px-3 py-1.5",
-        icon: "p-2",
+        default: "",
+        icon: "",
       },
     },
     defaultVariants: {
