1023c93774320f0c1bbaf6f51403253b00d53f28
Wan Aqim <wanaqim@gmail.com>
2024-06-26 11:32:35 +0800

    add more url endpoints

--- backend/ask_gov/urls.py
+++ backend/ask_gov/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import QuestionListCreateView, QuestionDetailView, AgencyListView, SubmitQuestionView, get_questions_by_agency
+from .views import QuestionListCreateView, QuestionDetailView, AgencyListView, SubmitQuestionView, QuestionsByAgencyView, LoginView, UserAgencyQuestionsView, SubmitAnswerView, UserAgencyTopicsView, AddTopicView
 
 urlpatterns = [
     path('questions/', QuestionListCreateView.as_view(), name='question-list-create'),
     path('questions/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
     path('agencies/', AgencyListView.as_view(), name='agency-list'),
     path('submit-question/<int:agency_id>/', SubmitQuestionView.as_view(), name='submit-question'),
-    path('questions-by-agency/<int:agency_id>/', get_questions_by_agency, name='questions-by-agency'),
+    path('questions/by-agency/<int:agency_id>/', QuestionsByAgencyView.as_view(), name='questions-by-agency'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('questions/user-agency/', UserAgencyQuestionsView.as_view(), name='user-agency-questions'),
+    path('questions/<int:question_id>/submit-answer/', SubmitAnswerView.as_view(), name='submit-answer'),
+    path('topics/user-agency/', UserAgencyTopicsView.as_view(), name='user-agency-topics'),
+    path('topics/add/', AddTopicView.as_view(), name='add-topic'),
 ]
