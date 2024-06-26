cd9831d1bc2571ae99ff52a67dafa200d87cd871
Wan Aqim <wanaqim@gmail.com>
2024-06-26 15:23:22 +0800

    add get all topics endpoint

--- backend/ask_gov/urls.py
+++ backend/ask_gov/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import QuestionListCreateView, QuestionDetailView, AgencyListView, SubmitQuestionView, QuestionsByAgencyView, LoginView, UserAgencyQuestionsView, SubmitAnswerView, UserAgencyTopicsView, AddTopicView
-
+from .views import QuestionListCreateView, QuestionDetailView, AgencyListView, SubmitQuestionView, QuestionsByAgencyView, LoginView, UserAgencyQuestionsView, SubmitAnswerView, UserAgencyTopicsView, AddTopicView, TopicListView
 urlpatterns = [
     path('questions/', QuestionListCreateView.as_view(), name='question-list-create'),
     path('questions/<int:pk>/', QuestionDetailView.as_view(), name='question-detail'),
@@ -12,4 +11,6 @@ urlpatterns = [
     path('questions/<int:question_id>/submit-answer/', SubmitAnswerView.as_view(), name='submit-answer'),
     path('topics/user-agency/', UserAgencyTopicsView.as_view(), name='user-agency-topics'),
     path('topics/add/', AddTopicView.as_view(), name='add-topic'),
+    path('topics/', TopicListView.as_view(), name='topics-list'),
+
 ]
