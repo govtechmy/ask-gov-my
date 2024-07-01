476cb030e6f1f141db8c3ad3cd1c6c5a187c4bde
Wan Aqim <wanaqim@gmail.com>
2024-07-01 12:21:32 +0800

    change the question interface and update elastic search client

--- frontend/actions/searchServices.ts
+++ frontend/actions/searchServices.ts
@@ -1,52 +1,44 @@
-'use server'
-import { Client } from '@elastic/elasticsearch-serverless';
+"use server";
+import { Client } from "@elastic/elasticsearch";
 
-const URL = "https://askgov-fc58f6.es.us-east-1.aws.elastic.cloud"
+const URL = "https://askgov-fc58f6.es.us-east-1.aws.elastic.cloud";
 
 const client = new Client({
-    node: URL,
-    auth: {
-        apiKey: "NWNHNVRaQUI3cVdKTXhCbHk4Sl86Um1LTmRKNjFSMjJXeUVtNGFVMEtldw=="
-    },
+  node: URL,
+  auth: {
+    apiKey: "NWNHNVRaQUI3cVdKTXhCbHk4Sl86Um1LTmRKNjFSMjJXeUVtNGFVMEtldw==",
+  },
 });
 
-const BACKEND_API_URL = "http://localhost:8000/api/questions/";
-
 interface Question {
-    id: number;
-    question: string;
-    agency: {
-        id: number;
-        name: string;
-        acronym: string | null;
-    };
-    date: string;
-    answer: string;
-    topics: Array<{
-        id: number;
-        title: string;
-    }>;
-    email: string;
+  id: number;
+  question: string;
+  date: string;
+  state: string;
+  agency: number;
+  answer: string;
+  topics: number[];
+  email: string;
 }
 
 export async function searchQuestions(query: string) {
-    try {
-        const result = await client.search({
-            index: 'questions',
-            size: 5, // limit to 5 results
-            body: {
-                query: {
-                    query_string: {
-                        query: `*${query}*`,
-                        fields: ['question', 'agency.name', 'topics.title', 'answer']
-                    }
-                }
-            }
-        });
+  try {
+    const result = await client.search({
+      index: "questions",
+      size: 5, // limit to 5 results
+      body: {
+        query: {
+          query_string: {
+            query: `*${query}*`,
+            fields: ["question", "topics.title", "answer"],
+          },
+        },
+      },
+    });
 
-        return result.hits.hits.map((hit: any) => hit._source);
-    } catch (error) {
-        console.error('Error searching questions:', error);
-        return [];
-    }
+    return result.hits.hits.map((hit: any) => hit._source);
+  } catch (error) {
+    console.error("Error searching questions:", error);
+    return [];
+  }
 }
