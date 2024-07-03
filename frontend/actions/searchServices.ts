2f91bd3b6bd67b15b5bb37c47bcd1f97e2654eb0
Wan Aqim <wanaqim@gmail.com>
2024-07-03 09:50:43 +0800

    change elasticsearch instance

--- frontend/actions/searchServices.ts
+++ frontend/actions/searchServices.ts
@@ -1,15 +1,19 @@
 "use server";
+import dotenv from 'dotenv';
 import { Client } from "@elastic/elasticsearch";
 import { AGENCY_TO_UUID } from "@/lib/agency";
+dotenv.config();
 
-const URL = "https://askgov-fc58f6.es.us-east-1.aws.elastic.cloud";
+const elasticsearchURL = process.env.ELASTICSEARCH_URL;
+const elasticsearchApiKey = process.env.ELASTICSEARCH_API_KEY;
 
 const client = new Client({
-  node: URL,
+  node: elasticsearchURL,
   auth: {
-    apiKey: "NWNHNVRaQUI3cVdKTXhCbHk4Sl86Um1LTmRKNjFSMjJXeUVtNGFVMEtldw==",
+    apiKey: elasticsearchApiKey,
   },
 });
+
 
 interface Question {
   id: number;
