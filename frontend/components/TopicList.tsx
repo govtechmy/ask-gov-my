312d69437ab1c88ae7af342e9c6c1479bbed7097
Wan Aqim <wanaqim@gmail.com>
2024-07-12 16:08:39 +0800

    fix topics selected from agencypage not redirecting correctly

--- frontend/components/TopicList.tsx
+++ frontend/components/TopicList.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useState, useEffect } from 'react';
+import { useTranslations } from 'next-intl';
 
 interface Topic {
   id: number;
@@ -13,16 +15,24 @@ interface TopicListProps {
 }
 
 const TopicList: React.FC<TopicListProps> = ({ topics, locale, selectedTopicId }) => {
+  const t = useTranslations('Topics');
+  const [currentPath, setCurrentPath] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setCurrentPath(window.location.pathname);
+    }
+  }, []);
+
   const constructHref = (topicId: number) => {
-    const currentPath = window.location.pathname;
-    return currentPath.includes('/topics/') //do ternary operator to check if already got 'topics' in url
+    return currentPath.includes('/topics/')
       ? currentPath.replace(/\/topics\/\d+$/, `/topics/${topicId}`)
       : `${currentPath}/topics/${topicId}`;
   };
 
   return (
     <div className="bg-white shadow-md rounded-lg p-4">
-      <h2 className="text-lg font-semibold mb-4">Topics</h2>
+      <h2 className="text-lg font-semibold mb-4">{t('topic')}</h2>
       <ul>
         {topics.map((topic) => (
           <li key={topic.id} className="mb-2">
