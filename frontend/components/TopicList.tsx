55cc0d3b11e80bba276f7e1630f2e9eb7f934230
Wan Aqim <wanaqim@gmail.com>
2024-07-12 08:57:35 +0800

    update topiclist component to assign href directly in topics

--- frontend/components/TopicList.tsx
+++ frontend/components/TopicList.tsx
@@ -13,27 +13,25 @@ interface TopicListProps {
 }
 
 const TopicList: React.FC<TopicListProps> = ({ topics, locale, selectedTopicId }) => {
-  const handleTopicClick = (topicId: number) => {
+  const constructHref = (topicId: number) => {
     const currentPath = window.location.pathname;
-    const newPath = currentPath.includes('/topics/')
+    return currentPath.includes('/topics/') //do ternary operator to check if already got 'topics' in url
       ? currentPath.replace(/\/topics\/\d+$/, `/topics/${topicId}`)
       : `${currentPath}/topics/${topicId}`;
-    window.location.href = newPath;
   };
 
   return (
     <div className="bg-white shadow-md rounded-lg p-4">
       <h2 className="text-lg font-semibold mb-4">Topics</h2>
       <ul>
-        <li className="mb-2"></li>
         {topics.map((topic) => (
           <li key={topic.id} className="mb-2">
-            <button
-              onClick={() => handleTopicClick(topic.id)}
+            <a
+              href={constructHref(topic.id)}
               className={`text-blue-500 hover:underline ${selectedTopicId === topic.id ? 'bg-purple-100' : ''}`}
             >
               {locale === 'ms' ? topic.title_ms : topic.title}
-            </button>
+            </a>
           </li>
         ))}
       </ul>
