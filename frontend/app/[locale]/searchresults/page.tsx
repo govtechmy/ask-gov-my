76ccc63a607aff4e67bfae356745443d270c9b84
Wan Aqim <wanaqim@gmail.com>
2024-07-14 13:29:51 +0800

    add translations for searchresults, mainpage and trending agencies

--- frontend/app/[locale]/searchresults/page.tsx
+++ frontend/app/[locale]/searchresults/page.tsx
@@ -6,6 +6,7 @@ import Footer from '@/components/FooterDetails/Footer';
 import Header from '@/components/HeaderDetails/Header';
 import TrendingAgencies from '@/components/TrendingAgencies';
 import IdentifyWebsite from '@/components/HeaderDetails/IdentifyWebsite';
+import WordTranslate from '@/components/WordTranslate';
 
 const SearchResultPage = async ({
   searchParams,
@@ -34,15 +35,18 @@ const SearchResultPage = async ({
         <div className="container mt-10 flex text-out">
           <div className="max-w-screen-2xl">
             <div className="font-semibold text-base text-black-700 pb-7 flex">
-              <div>{questions.length} search results for &nbsp;</div>
-              <div>{query}</div>
+                {questions.length}
+              <div>
+              <WordTranslate translate={'Search'} keyword={'searchresult'}></WordTranslate>
+              </div>
+              <div> "{query}"</div>
             </div>
             <QuestionBox questions={questions} />
           </div>
 
           <div className="pl-10 w-[500px]">
             <div className="font-semibold text-base text-black-700">
-              Relevant Agencies
+            <WordTranslate translate={'Mainpage'} keyword={'trendingA'}></WordTranslate>
             </div>
             <TrendingAgencies agencies={trendingAgencies} />
           </div>
