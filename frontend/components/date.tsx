432f28fc7fae9cfe3cad545de76ce7ca8411670f
Wan Aqim <wanaqim@gmail.com>
2024-07-15 15:05:28 +0800

    update date component to change based on locale

--- frontend/components/date.tsx
+++ frontend/components/date.tsx
@@ -1,18 +1,24 @@
 interface Props {
   date: string;
+  locale: string;
 }
 
-const formatDateDifference = (dateString: string): string => {
-  const date = new Date(dateString);
-  const today = new Date();
 
-  const diffTime = Math.abs(today.getTime() - date.getTime());
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-  return `${diffDays}d ago`;
-};
+const DateComponent: React.FC<Props> = ({ date, locale }) => {
+  const formatDateDifference = (dateString: string): string => {
+    const date = new Date(dateString);
+    const today = new Date();
+  
+    const diffTime = Math.abs(today.getTime() - date.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    if (locale === 'ms') {
+      return `${diffDays}h lepas`;
+    } else {
+      return `${diffDays}d ago`;
+    }
+  };
 
-const DateComponent: React.FC<Props> = ({ date }) => {
   const formattedDate = formatDateDifference(date);
 
   return <span>{formattedDate}</span>;
