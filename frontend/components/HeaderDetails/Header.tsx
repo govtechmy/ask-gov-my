83280c30077d704dfe717bb4066f7fe83ddc7149
harrisazmi <azmi.harris@gmail.com>
2024-07-15 14:04:04 +0800

    link header repaired

--- frontend/components/HeaderDetails/Header.tsx
+++ frontend/components/HeaderDetails/Header.tsx
@@ -1,16 +1,20 @@
+'use client';
 import ThemeToggle from './Theme';
 import LocaleSwitch from './LocaleSwitch';
 import Asklogo from '@/icons/asklogo';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className="w-full bg-white p-2 border-[1px] border-outline-200">
       <div className="container flex mx-auto">
         <div className="flex justify-between w-full">
-          <div className="font-poppins flex h-full gap-2.5 text-lg font-semibold items-center">
-            <Asklogo />
-            AskMyGov
-          </div>
+          <Link href="/">
+            <div className="font-poppins flex h-full gap-2.5 text-lg font-semibold items-center hover:cursor-pointer">
+              <Asklogo />
+              AskMyGov
+            </div>
+          </Link>
           <div className="flex gap-3">
             <ThemeToggle />
             <LocaleSwitch />
