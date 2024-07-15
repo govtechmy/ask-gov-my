0e8858dc58f9585dfec48e47192c08b7adf21486
harrisazmi <azmi.harris@gmail.com>
2024-07-15 13:03:55 +0800

    edited identify website, using mashead as ref

--- frontend/components/HeaderDetails/IdentifyWebsite.tsx
+++ frontend/components/HeaderDetails/IdentifyWebsite.tsx
@@ -1,82 +1,80 @@
 'use client';
 
-import React, { useState } from 'react';
-import Image from 'next/image';
+import Collapse from '../ui/collapse';
+import TickWithRugged from '@/icons/tickwithrugged';
+import ChevronDown from '@/icons/ChevronDown';
+import PutrajayaIcon from '@/icons/putrajaya';
+import EncryptedLock from '@/icons/encryptedlock';
+import SolidLock from '@/icons/solidlock';
+import { cn } from '@/lib/utils';
+import { useTranslations } from 'next-intl';
+import { useState } from 'react';
 
-const IdentifyWebsite: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+export default function Masthead() {
+  const t = useTranslations('Masthead');
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="bg-white sticky top-0 z-10 px-3 py-1">
-      <div
-        className="flex w-full cursor-pointer"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="inline-flex">
-          <div className="flex flex-wrap items-center text-xs">
-            <Image
-              src="/jata_logo.png"
-              width={25}
-              height={25}
-              alt="Logo Jata Negara"
-            />
-            <span className="px-3">A Malaysian Government Agency Website</span>
-            <span className="items-center text-blue-600 underline">
-              How to identify Malaysian Government Website?
-            </span>
-          </div>
-        </div>
-      </div>
-      {isOpen && (
-        <div
-          className="mt-2 w-full rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="menu-button"
-          tabIndex={-1}
-        >
-          <div className="py-1" role="none">
-            <div id="official-links" className="mb-4 rounded-md p-4">
-              <div className="flex items-start">
-                <div className="mr-2 text-xl">🏛️</div>
-                <div>
-                  <h2 className="text-lg font-semibold">
-                    Official website links end with .gov.my
-                  </h2>
-                  <p className="text-gray-700">
-                    Government agencies communicate via .gov.my websites (e.g.
-                    go.gov.my).
-                    <a
-                      href="https://trusted-websites.gov.my"
-                      className="text-blue-600 underline"
-                    >
-                      Trusted websites
-                    </a>
-                    {/* REWRITE BACK THIS PLACEHOLDER FOR TRUSTED WEBSITES FROM GOV */}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div id="secure-websites" className="rounded-md p-4">
-              <div className="flex items-start">
-                <div className="mr-2 text-xl">🔒</div>
-                <div>
-                  <h2 className="text-lg font-semibold">
-                    Secure websites use HTTPS
-                  </h2>
-                  <p className="text-gray-700">
-                    Look for a lock (🔒) or https:// as an added precaution.
-                    Share sensitive information only on official, secure
-                    websites.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div
+      className={cn(
+        'z-[99]',
+        open
+          ? 'bg-gradient-to-b from-washed-100 from-[84.74%] to-outline-200 to-100%'
+          : 'bg-gradient-to-b from-washed-100 from-[84.74%] to-outline-200 to-100%',
       )}
+    >
+      <div className="container">
+        <button className="w-full" onClick={() => setOpen(!open)}>
+          <div className="flex flex-wrap items-center gap-1.5 py-2.5 text-sm/4 text-brand-700 max-sm:justify-between sm:py-1">
+            <div className="flex items-center gap-1.5">
+              <TickWithRugged className="size-4 sm:size-5 stroke-[#1D4ED8] dark:stroke-[#588BFB]" />
+              <span className="text-black-700">
+                {t('official_gov_website')}
+              </span>
+            </div>
+            <div className="flex items-center gap-0.5 max-sm:rounded-md max-sm:bg-outline-200 max-sm:px-1">
+              <span className="hidden tracking-[-0.01em] sm:block">
+                {t('how_to_identify')}
+              </span>
+              <ChevronDown
+                className={cn('size-4 transition', open ? 'rotate-180' : '')}
+              />
+            </div>
+          </div>
+        </button>
+        <Collapse isOpen={open}>
+          <div className="grid grid-cols-1 gap-4.5 pb-6 pt-4.5 sm:grid-cols-2 sm:gap-6 sm:pb-8 sm:pt-6">
+            <span className="static text-sm text-brand-700 sm:hidden">
+              {t('how_to_identify')}
+            </span>
+
+            <div className="flex gap-3">
+              <PutrajayaIcon className="shrink-0 text-dim-500" />
+              <div className="space-y-1.5">
+                <p className="font-medium max-sm:text-sm">{t('official')}</p>
+                <p className="max-w-prose text-balance text-sm text-black-700">
+                  {t('not_govmy')}
+                  <span className="font-semibold">.gov.my</span>
+                  {t('close_site')}
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-3">
+              <EncryptedLock className="shrink-0 text-dim-500" />
+              <div className="space-y-1.5">
+                <p className="font-medium max-sm:text-sm">{t('secure')}</p>
+                <div className="max-w-prose text-balance text-sm text-black-700">
+                  {t('find_lock')}{' '}
+                  <SolidLock className="-ml-[3px] mb-0.5 mr-px inline size-3.5" />
+                  {t('or')}
+                  <span className="font-semibold">https://</span>
+                  {t('precaution')}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Collapse>
+      </div>
     </div>
   );
-};
-
-export default IdentifyWebsite;
+}
