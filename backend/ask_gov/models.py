1afa647542a3118fded365e924233597cd5aaf0f
Wan Aqim <wanaqim@gmail.com>
2024-07-17 15:27:38 +0800

    add agencylogo field to models and views.py

--- backend/ask_gov/models.py
+++ backend/ask_gov/models.py
@@ -6,6 +6,7 @@ class Agency(models.Model):
     name = models.CharField(max_length=255)
     name_ms = models.CharField(max_length=255, null=True, blank=True)
     acronym = models.CharField(max_length=50, null=True, blank=True)
+    logo_url = models.URLField(max_length=500, null=True, blank=True)  # New field for the logo URL
 
     def __str__(self):
         return self.name
