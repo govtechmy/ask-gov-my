b3f51cd82ab93b0bf11f8c50bd884bdedc604314
harrisazmi <azmi.harris@gmail.com>
2024-07-18 17:55:20 +0800

    repaired the color and the navpath and adjust the Dynamic path name

--- frontend/components/HeaderDetails/HeaderAgency.tsx
+++ frontend/components/HeaderDetails/HeaderAgency.tsx
@@ -27,7 +27,7 @@ const HeaderAgency: React.FC<HeaderAgencyProps> = ({ agencyAcronym }) => {
       <div className="w-full bg-white p-2 border-[1px] border-outline-200 h-16 flex items-center">
         <div className="container flex">
           <div className="flex justify-between w-full items-center">
-            <Link href="/">
+            <Link href={`/${agencyAcronym}`}>
               <div className="font-poppins flex h-full gap-2.5 text-lg font-semibold items-center hover:cursor-pointer">
                 <Asklogo />
                 <div className="flex">
