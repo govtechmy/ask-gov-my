b38720c0a46fe38d0358a1e017bf9250a64806d9
Wan Aqim <wanaqim@gmail.com>
2024-07-22 11:28:40 +0800

    rename field

--- backend/update_answered_date.py
+++ backend/update_answered_date.py
@@ -11,7 +11,7 @@ from ask_gov.models import Question
 def update_answered_date():
     questions = Question.objects.all()
     for question in questions:
-        question.answeredDate = question.date
+        question.answered_date = question.date
         question.save()
         print(f"Updated Question ID {question.id} with answeredDate {question.answeredDate}")
 
