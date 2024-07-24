531df105b6f72b5a6a50d6506f18c2870272d5ee
Wan Aqim <wanaqim@gmail.com>
2024-07-24 16:17:48 +0800

    update bug

--- frontend/components/AdminDashboard/AdminQuestionCard.tsx
+++ frontend/components/AdminDashboard/AdminQuestionCard.tsx
@@ -70,10 +70,9 @@ const AdminQuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
   };
 
   const handleCardClick = async () => {
+    setIsModalOpen(true);
     if (!question.admin_isopen) {
       try {
-        setIsModalOpen(true);
-
         await changeAdminIsOpen(question.id);
         question.admin_isopen = true;
       } catch (error) {
@@ -101,11 +100,8 @@ const AdminQuestionCard: React.FC<QuestionCardProps> = ({ question }) => {
 
   return (
     <>
-      <div className="bg-white items-center rounded-md border p-4 shadow-sm flex justify-between cursor-pointer w-full">
-        <div
-          className="flex items-center "
-          onClick={() => setIsModalOpen(true)}
-        >
+      <div className="bg-white items-center rounded-md border p-4 shadow-sm flex justify-between cursor-pointer w-full" onClick={() => handleCardClick()}>
+        <div className="flex items-center ">
           <div className="text-sm font-medium text-black-700 line-clamp-2">
             {question.question}
           </div>
