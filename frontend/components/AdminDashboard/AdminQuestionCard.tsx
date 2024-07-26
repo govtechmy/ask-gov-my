e8e80a4c991d354758b8cf2ac32321c50ca0bf76
harrisazmi <azmi.harris@gmail.com>
2024-07-26 11:47:22 +0800

    repair how three dot hover works

--- frontend/components/AdminDashboard/AdminQuestionCard.tsx
+++ frontend/components/AdminDashboard/AdminQuestionCard.tsx
@@ -74,12 +74,6 @@ const AdminQuestionCard: React.FC<QuestionCardProps> = ({
     );
   };
 
-  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-
-  const handleClick = () => {
-    setIsDropdownVisible(prevState => !prevState);
-  };
-
   const handleMarkAsSpamToast = () => {
     setShowSpamToast(true);
   };
@@ -93,7 +87,7 @@ const AdminQuestionCard: React.FC<QuestionCardProps> = ({
 
   return (
     <>
-      <div className="bg-white items-center rounded-md border p-4 shadow-sm flex justify-between w-full">
+      <div className="bg-white items-center rounded-md border p-4 shadow-sm flex justify-between w-full group">
         <div className="flex items-center">
           <div
             className="text-sm font-medium text-black-700 line-clamp-2 hover:cursor-pointer"
--- frontend/components/AdminDashboard/ThreeDottedMarkAsSpam.tsx
+++ frontend/components/AdminDashboard/ThreeDottedMarkAsSpam.tsx
@@ -31,10 +31,10 @@ const ThreeDottedMarkAsSpam: React.FC<ThreeProps> = ({
 
   return (
     <div className="relative">
-      <div className="group flex relative">
+      <div className="flex relative">
         <div
           className={`opacity-0 ${isDropdownVisible ? 'opacity-100' : 'group-hover:opacity-100 group-hover:cursor-pointer'}
-          h-8 w-8 border-[1px] border-outline-200 bg-white rounded-lg shadow-button cursor-pointer
+          h-8 w-8 border-[1px] border-outline-200 bg-white rounded-lg shadow-button curs
           items-center justify-center absolute flex top-[-16px] right-[-6px]`}
           onClick={handleDropdownClick}
         >
