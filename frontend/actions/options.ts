dfcb5efea75b4c85b8be0ad82c9e1fffdd75400d
Wan Aqim <wanaqim@gmail.com>
2024-08-06 08:17:54 +0800

    refactor next-auth to use http api to django

--- frontend/actions/options.ts
+++ frontend/actions/options.ts
@@ -1,33 +1,122 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import { NextAuthOptions } from 'next-auth';
-import { sendEmail } from './sendMail';
-import LoginLink from './logic-link';
-import { checkUserEmailExists } from './userServices';
+import { get, post } from '@/lib/api';
+import type {
+  Adapter,
+  AdapterUser,
+  VerificationToken,
+  AdapterSession,
+  AdapterAccount
+} from "@auth/core/adapters";
+const API_URL = process.env.API_URL;
 
-const prisma = new PrismaClient();
+const DjangoAdapter = (): Adapter => {
+  return {
+    async createVerificationToken(verificationToken: VerificationToken) {
+      return await post(`${API_URL}/api/auth/verification`, verificationToken);
+    },
+
+    async useVerificationToken({ identifier, token }) {
+      return await post(`${API_URL}/api/auth/verification`, { identifier, token });
+    },
+
+    async createUser(user: Omit<AdapterUser, "id">): Promise<AdapterUser> {
+      const createdUser = await post<AdapterUser>(`${API_URL}/api/auth/user`, user);
+      if (!createdUser) {
+        throw new Error('Failed to create user');
+      }
+      return createdUser;
+    },
+
+    async getUser(id) {
+      return await get(`${API_URL}/api/auth/user`, { id });
+    },
+
+    async getUserByEmail(email) {
+      return await get(`${API_URL}/api/auth/user`, { email });
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      return await get(`${API_URL}/api/auth/user`, { providerAccountId, provider });
+    },
+
+    async updateUser(user: Partial<AdapterUser> & Pick<AdapterUser, "id">): Promise<AdapterUser> {
+      const updatedUser = await post<AdapterUser>(`${API_URL}/api/auth/user`, user);
+      if (!updatedUser) {
+        throw new Error('Failed to update user');
+      }
+      return updatedUser;
+    },
+
+    async linkAccount(account: AdapterAccount): Promise<AdapterAccount> {
+      const linkedAccount = await post<AdapterAccount>(`${API_URL}/api/auth/account`, account);
+      if (!linkedAccount) {
+        throw new Error('Failed to link account');
+      }
+      return linkedAccount;
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const session_and_user = await get<{ session: AdapterSession; user: AdapterUser; } | null>(
+        `${API_URL}/api/auth/session`, 
+        { sessionToken }
+      );
+      if (!session_and_user) return null;
+      return {
+        session: {
+          ...session_and_user.session,
+          expires: new Date(session_and_user.session.expires),
+        },
+        user: session_and_user.user,
+      };
+    },
+
+    async createSession({ sessionToken, userId, expires }): Promise<AdapterSession> {
+      const session = await post<AdapterSession>(`${API_URL}/api/auth/session`, { sessionToken, userId, expires });
+      if (!session) {
+        throw new Error('Failed to create session');
+      }
+      return {
+        userId: session.userId,
+        sessionToken: session.sessionToken,
+        expires: new Date(session.expires),
+      };
+    },
+
+    async updateSession(session: Partial<AdapterSession> & Pick<AdapterSession, "sessionToken">): Promise<AdapterSession | null> {
+      const updatedSession = await post<AdapterSession>(`${API_URL}/api/auth/session`, session);
+      if (!updatedSession || !updatedSession.sessionToken || !updatedSession.userId || !updatedSession.expires) {
+        throw new Error('Failed to update session or invalid response from API');
+      }
+      return {
+        userId: updatedSession.userId,
+        sessionToken: updatedSession.sessionToken,
+        expires: new Date(updatedSession.expires),
+      };
+    },
+    
+    async deleteSession(sessionToken) {
+      await post(`${API_URL}/api/auth/session`, { sessionToken });
+    },
+
+    async unlinkAccount(partialAccount) {
+      await post(`${API_URL}/api/auth/account`, partialAccount);
+    },
+  };
+};
 
 export const authOptions: NextAuthOptions = {
   providers: [
     EmailProvider({
       async sendVerificationRequest({ identifier, url }) {
-        const emailExists = await checkUserEmailExists(identifier);
-        if (emailExists) {
-          // await sendEmail({
-          //   email: identifier,
-          //   subject: `Your ${process.env.NEXT_PUBLIC_APP_NAME} Login Link`,
-          //   react: LoginLink({ url, email: identifier }),
-          // });
-          console.log(url);
-        } else {
-          console.error('Email not found in the database.');
-        }
+        //create function to send the magic link to django
+        //django will send the magic link to the users
+        console.log(`Magic link URL: ${url}`);
       },
     }),
   ],
-  adapter: PrismaAdapter(prisma),
+  adapter: DjangoAdapter(),
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: '/admin',
@@ -38,15 +127,6 @@ export const authOptions: NextAuthOptions = {
     strategy: 'jwt',
   },
   callbacks: {
-    async session({ session, token }) {
-      if (token) {
-        session.user.id = token.id as string;
-        session.user.role = token.role as string;
-        session.user.agency = token.agency as string;
-        session.accessToken = token.accessToken as string;
-      }
-      return session;
-    },
     async jwt({ token, user, account }) {
       if (user) {
         token.id = user.id;
