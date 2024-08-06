cf18b0f3c537ee17560aa0a148c13715711bb049
Wan Aqim <wanaqim@gmail.com>
2024-08-06 08:17:54 +0800

    refactor next-auth to use http api to django

---
+++ frontend/lib/api.ts
@@ -0,0 +1,59 @@
+const default_option: RequestInit = {
+    headers: { "Content-Type": "application/json" },
+    cache: "no-store",
+  };
+  
+  export const post = async <T>(
+    path: string,
+    payload: Record<string, any>,
+    option: RequestInit = default_option
+  ) =>
+    new Promise<T | null>(async (resolve, reject) => {
+      fetch(path, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        ...option,
+      })
+        .then((response) => {
+  
+          // Check if the response body is null
+          if (response.body === null) {
+            resolve(null);
+          }
+  
+          resolve(response.json() as T);
+        })
+        .catch((error) => reject(error));
+    });
+
+    export const get = async <T>(
+  path: string,
+  payload?: Record<string, any>,
+  option: RequestInit = default_option
+) =>
+  new Promise<T | null>(async (resolve, reject) => {
+    fetch(`${path}?${new URLSearchParams(payload).toString()}`, {
+      method: "GET",
+      ...option,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === HttpStatusCode.NO_CONTENT_204) return null;
+          else throw new Error("Network response was not ok");
+        }
+        // Check if the response body is null
+        if (response.body === null) {
+          resolve(null);
+        }
+        try {
+          const item = response.json() as T;
+          resolve(item);
+        } catch (error) {
+          resolve(response.body as T);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
