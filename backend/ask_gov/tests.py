4c886d761eccd804dab826fbf60a47a1c018a38e
juwaini <juwaini@gmail.com>
2024-08-07 15:20:43 +0800

    update question

--- backend/ask_gov/tests.py
+++ backend/ask_gov/tests.py
@@ -2,20 +2,24 @@ from django.test import TestCase
 from django.urls import reverse
 
 from faker import Faker
-from ask_gov.models import CustomUser, Question
+from ask_gov.models import User, Question
 
 fake = Faker()
+
+
 def create_accounts():
     pass
 
 
 class TestAskGov(TestCase):
     def setUp(self):
-        CustomUser.objects.create_user(username='test_user', password='test786')
-        CustomUser.objects.create_superuser(username='testsuperuser', password='test5579')
+        user_email = fake.email()
+        superuser_email = fake.email()
+        User.objects.create_user(username= user_email.split('@')[0], email=user_email, password='test786')
+        User.objects.create_superuser(username= superuser_email.split('@')[0], email=superuser_email, password='test5579')
 
     def test_setup_working(self):
-        self.assertEqual(CustomUser.objects.count(), 2)
+        self.assertEqual(User.objects.count(), 2)
 
     def test_get_all_questions(self):
         url = reverse('question-list-create')
@@ -28,7 +32,7 @@ class TestAskGov(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_question_detail(self):
-        Question.objects.create(question=f'{fake.bs()}?', email=fake.email())
-        url = reverse('question-detail', kwargs={'pk': 1})
+        question = Question.objects.create(question=f'{fake.bs()}?', email=fake.email())
+        url = reverse('question-detail', kwargs={'pk': question.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
