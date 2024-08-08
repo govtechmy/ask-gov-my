739ca3165f139b19fa12648a2f95d224e4e56854
Wan Aqim <wanaqim@gmail.com>
2024-08-08 16:48:42 +0800

    Co-authored-by: PaanJoe <paanjoe@users.noreply.github.com>

--- frontend/actions/options.ts
+++ frontend/actions/options.ts
@@ -14,15 +14,15 @@ const API_URL = process.env.API_URL;
 const DjangoAdapter = (): Adapter => {
   return {
     async createVerificationToken(verificationToken: VerificationToken) {
-      return await post(`${API_URL}/api/auth/verification`, verificationToken);
+      return await post(`${API_URL}/auth/verification`, verificationToken);
     },
 
     async useVerificationToken({ identifier, token }) {
-      return await put(`${API_URL}/api/auth/verification`, { identifier, token });
+      return await put(`${API_URL}/auth/verification`, { identifier, token });
     },
 
     async createUser(user: Omit<AdapterUser, "id">): Promise<AdapterUser> {
-      const createdUser = await post<AdapterUser>(`${API_URL}/api/auth/user`, user);
+      const createdUser = await post<AdapterUser>(`${API_URL}/auth/user`, user);
       if (!createdUser) {
         throw new Error('Failed to create user');
       }
@@ -30,19 +30,19 @@ const DjangoAdapter = (): Adapter => {
     },
 
     async getUser(id) {
-      return await get(`${API_URL}/api/auth/user`, { id });
+      return await get(`${API_URL}/auth/user`, { id });
     },
 
     async getUserByEmail(email) {
-      return await get(`${API_URL}/api/auth/user`, { email });
+      return await get(`${API_URL}/auth/user`, { email });
     },
 
     async getUserByAccount({ providerAccountId, provider }) {
-      return await get(`${API_URL}/api/auth/user`, { providerAccountId, provider });
+      return await get(`${API_URL}/auth/user`, { providerAccountId, provider });
     },
 
     async updateUser(user: Partial<AdapterUser> & Pick<AdapterUser, "id">): Promise<AdapterUser> {
-      const updatedUser = await put<AdapterUser>(`${API_URL}/api/auth/user`, user);
+      const updatedUser = await put<AdapterUser>(`${API_URL}/auth/user`, user);
       if (!updatedUser) {
         throw new Error('Failed to update user');
       }
@@ -50,7 +50,7 @@ const DjangoAdapter = (): Adapter => {
     },
 
     async linkAccount(account: AdapterAccount): Promise<AdapterAccount> {
-      const linkedAccount = await post<AdapterAccount>(`${API_URL}/api/auth/account`, account);
+      const linkedAccount = await post<AdapterAccount>(`${API_URL}/auth/account`, account);
       if (!linkedAccount) {
         throw new Error('Failed to link account');
       }
@@ -59,7 +59,7 @@ const DjangoAdapter = (): Adapter => {
 
     async getSessionAndUser(sessionToken) {
       const session_and_user = await get<{ session: AdapterSession; user: AdapterUser; } | null>(
-        `${API_URL}/api/auth/session`, 
+        `${API_URL}/auth/session`, 
         { sessionToken }
       );
       if (!session_and_user) return null;
@@ -74,7 +74,7 @@ const DjangoAdapter = (): Adapter => {
     },
 
     async createSession({ sessionToken, userId, expires }): Promise<AdapterSession> {
-      const session = await post<AdapterSession>(`${API_URL}/api/auth/session`, { sessionToken, userId, expires });
+      const session = await post<AdapterSession>(`${API_URL}/auth/session`, { sessionToken, userId, expires });
       if (!session) {
         throw new Error('Failed to create session');
       }
@@ -86,7 +86,7 @@ const DjangoAdapter = (): Adapter => {
     },
 
     async updateSession(session: Partial<AdapterSession> & Pick<AdapterSession, "sessionToken">): Promise<AdapterSession | null> {
-      const updatedSession = await put<AdapterSession>(`${API_URL}/api/auth/session`, session);
+      const updatedSession = await put<AdapterSession>(`${API_URL}/auth/session`, session);
       if (!updatedSession || !updatedSession.sessionToken || !updatedSession.userId || !updatedSession.expires) {
         throw new Error('Failed to update session or invalid response from API');
       }
@@ -98,11 +98,11 @@ const DjangoAdapter = (): Adapter => {
     },
     
     async deleteSession(sessionToken) {
-      await post(`${API_URL}/api/auth/session`, { sessionToken });
+      await post(`${API_URL}/auth/session`, { sessionToken });
     },
 
     async unlinkAccount(partialAccount) {
-      await post(`${API_URL}/api/auth/account`, partialAccount);
+      await post(`${API_URL}/auth/account`, partialAccount);
     },
   };
 };
@@ -136,7 +136,7 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
     async redirect({ url, baseUrl }) {
-      if (url.startsWith(baseUrl)) return url;
+      if (url.startsWith(baseUrl)) return urcll;
       if (url.startsWith('/admin/dashboard')) return baseUrl + url;
       return baseUrl;
     },
