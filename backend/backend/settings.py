913eb3dbf72d4460e1f1d9dbc06ae13cf8478b08
juwaini <juwaini@gmail.com>
2024-08-09 08:29:05 +0800

    revert settings to default

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,12 +140,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "ask_gov.User"
-
-CSRF_TRUSTED_ORIGINS = ['*']  # FIXME
