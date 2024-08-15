01a76428d609b02123df8e08738d0ffe7ef11c00
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2024-08-15 14:48:14 +0800

    wip: refactoring-tiptap

--- frontend/components/ReadOnlyTipTap.tsx
+++ frontend/components/ReadOnlyTipTap.tsx
@@ -2,15 +2,8 @@
 
 import { useEditor, EditorContent, Editor, JSONContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import { buttonVariants } from './ui/button'
-import { BoldIcon, BulletedListIcon, ItalicIcon, LinkIcon, OrderedListIcon, RedoIcon, StrikethroughIcon, UnderlineIcon, UndoIcon } from '@/icons/editor';
 import Underline from '@tiptap/extension-underline';
-import History from '@tiptap/extension-history';
 import Link from '@tiptap/extension-link';
-import { useCallback } from 'react';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
-import { Separator } from './ui/separator';
-
 
 export default ({
     editorText,
