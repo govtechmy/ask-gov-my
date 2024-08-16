98819da7fce6253df5f35d16d6bc872e3a3269d4
harrisazmi <azmi.harris@gmail.com>
2024-08-16 16:37:05 +0800

    repaired footer responsive

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer: React.FC<FooterProps> = ({ adminpage = false }) => {
   );
 
   const Links: React.FC = () => (
-    <>
+    <div>
       <a
         href="#"
         className="text-sm text-black-700 hover:text-black hover:underline"
@@ -34,33 +34,35 @@ const Footer: React.FC<FooterProps> = ({ adminpage = false }) => {
       </a>
       <a
         href={adminpage ? '#' : '/admin'}
-        className="text-sm text-black-700 hover:text-black hover:underline pl-5 sm:pl-0"
+        className="text-sm text-black-700 hover:text-black hover:underline pl-5"
       >
         {adminpage ? t('api_docs') : t('admin_login')}
       </a>
-    </>
+    </div>
   );
 
   if (adminpage) {
     return (
-      <div className="justify-between px-8 py-6 bg-white border-[1px] border-outline-200 sm:py-3 sm:flex">
-        <div className="flex items-center">
-          <div className="h-[30px] w-[30px]">
-            <Logo size={96} />
-          </div>
-          <div className="sm:flex items-center">
-            <p className="font-poppins whitespace-nowrap font-semibold text-sm pl-[10px]">
-              {t('gov_mys')}
-            </p>
-            <span className="px-2 hidden sm:block">
-              <Copyright />
-            </span>
-          </div>
-        </div>
-        <div className="flex items-center py-3 gap-4.5">
+      <div className="justify-between px-8 py-6 sm:py-3 sm:flex">
+        <div className="items-center py-3 gap-4.5 sm:hidden ">
           <Links />
         </div>
-        <div className="md:hidden pt-3">
+        <div className="flex items-center">
+          <div className="h-[30px] w-[30px] flex-shrink-0">
+            <Logo size={96} />
+          </div>
+          <p className="font-poppins whitespace-nowrap font-semibold text-sm pl-[10px]">
+            {t('gov_mys')}
+          </p>
+          <span className="px-2 hidden sm:block">
+            <Copyright />
+          </span>
+        </div>
+
+        <div className=" items-center py-3 gap-4.5 hidden sm:block">
+          <Links />
+        </div>
+        <div className="pt-3 sm:hidden">
           <Copyright />
         </div>
       </div>
