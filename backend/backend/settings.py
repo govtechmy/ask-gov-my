dcf00d5419eaa9165f6c17de1322334ccae58651
Wan Aqim <wanaqim@gmail.com>
2024-08-22 11:39:46 +0800

    remove DEFAULT_AUTHENTICATION_CLASSES

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,9 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 10
 }
