36f21d798cca4b2a26b367971747a6cec1c251dd
Wan Aqim <wanaqim@gmail.com>
2024-08-27 09:22:35 +0800

    enable pagination class in REST_FRAMEWORK settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,11 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    # 'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 # Internationalization
