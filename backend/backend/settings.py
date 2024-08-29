94de918d98204903b87a81e5272a8b2c60959a8c
Wan Aqim <wanaqim@gmail.com>
2024-08-29 10:53:07 +0800

    install django_filters apps in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "ask_gov",
     "rest_framework",
+    "django_filters",
     "rest_framework_simplejwt",
     "corsheaders",
     'django_extensions',
