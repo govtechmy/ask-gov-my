cb2ae217904ba54b86498a4040c7a653e5a2a802
Wan Aqim <wanaqim@gmail.com>
2024-09-02 08:07:23 +0800

    refactor QuestionBox to receive pagination state as 1-prop and remove function to fetch new questions when the page changes

--- frontend/components/common/QuestionBox/QuestionBox.tsx
+++ frontend/components/common/QuestionBox/QuestionBox.tsx
@@ -1,53 +1,36 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import QuestionCard from './QuestionCard';
 import Pagination from '@/components/ui/pagination';
 import { Question, Agency } from '@/types/types';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from '@/lib/i18n';
 
-interface QuestionBoxProps {
-  questions: Question[];
-  agencyMap: Record<string, string>;
-  agencyList: Agency[];
+interface PaginatedQuestions {
+  data: Question[];
   totalItems: number;
   totalPages: number;
   currentPage: number;
-  importFunction: Function;
-  value?: number | string;
-  secondValue?: string;
+}
+
+interface QuestionBoxProps {
+  questions: PaginatedQuestions;
+  agencyMap: Record<string, string>;
+  agencyList: Agency[];
 }
 
 const QuestionBox: React.FC<QuestionBoxProps> = ({
-  questions: initialQuestions,
+  questions,
   agencyMap,
   agencyList,
-  currentPage,
-  totalPages,
-  importFunction,
-  value,
-  secondValue,
 }) => {
-  const [questions, setQuestions] = useState<Question[]>(initialQuestions);
+  const { data, totalItems, currentPage, totalPages } = questions;
   const searchParams = useSearchParams();
   const router = useRouter();
-  
-  const itemsPerPage = 6;
 
-  const handlePageChange = async (page: number) => {
+  const handlePageChange = (page: number) => {
     if (page >= 1 && page <= totalPages) {
-      const {
-        questions: newQuestions,
-        totalItems: newTotalItems,
-        totalPages: newTotalPages,
-      } = secondValue
-        ? await importFunction(value, secondValue, page, itemsPerPage)
-        : value
-          ? await importFunction(value, page, itemsPerPage)
-          : await importFunction(page, itemsPerPage);
-      
-      setQuestions(newQuestions);
       const params = new URLSearchParams(searchParams.toString());
       params.set('page', page.toString());
       router.push(`${window.location.pathname}?${params.toString()}`);
@@ -57,7 +40,7 @@ const QuestionBox: React.FC<QuestionBoxProps> = ({
   return (
     <div className="flex flex-col justify-center gap-4">
       <div className="flex flex-col gap-6">
-        {questions.map(question => (
+        {data.map(question => (
           <QuestionCard
             key={question.id}
             question={question}
