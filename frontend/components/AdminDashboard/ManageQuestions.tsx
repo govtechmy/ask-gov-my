487007a9d981dc5fb5ccde136c3f429fe728aa4a
Wan Aqim <wanaqim@gmail.com>
2024-09-03 10:10:32 +0800

    refactor ManageQuestions into server component and extract Page, SearchTerm and selectedTab from params.

--- frontend/components/AdminDashboard/ManageQuestions.tsx
+++ frontend/components/AdminDashboard/ManageQuestions.tsx
@@ -1,61 +1,41 @@
-'use client';
-
-import { useTranslations } from 'next-intl';
-import { useState, useEffect } from 'react';
 import { getAllUserQuestions } from '@/actions/userServices';
+import { getAgencyList, getDynamicAgencyMap } from '@/actions/questionServices';
 import AdminQuestionBox from '@/components/AdminDashboard/AdminQuestionBox';
 import QuestionNavbar from '@/components/AdminDashboard/QuestionNavbar';
-import { Question } from '@/types/types';
+import { Question, Agency } from '@/types/types';
 
-const ManageQuestions: React.FC = () => {
-  const t = useTranslations('Adminlogin');
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [filteredQuestions, setFilteredQuestions] = useState<Question[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const [searchTerm, setSearchTerm] = useState('');
+interface ManageQuestionsProps {
+  searchParams: {
+    page?: string;
+    tab?: string;
+    searchTerm?: string;
+  };
+}
 
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const { questions } = await getAllUserQuestions();
-        setQuestions(questions);
-        setFilteredQuestions(questions); // Initialize filteredQuestions with all questions
-      } catch (error) {
-        setError(
-          error instanceof Error ? error.message : 'An unknown error occurred',
-        );
-      } finally {
-        setLoading(false);
-      }
-    };
+const ManageQuestions = async ({ searchParams }: ManageQuestionsProps) => {
+  const currentPage = searchParams.page ? parseInt(searchParams.page, 10) : 1;
+  const searchTerm = searchParams.searchTerm || '';
+  const tab = searchParams.tab || 'all';
 
-    fetchQuestions();
-  }, []); // No dependencies, so it only runs once on mount
-
-  useEffect(() => {
-    const filteredQuestions = questions.filter(q =>
-      q.question.toLowerCase().includes(searchTerm.toLowerCase()),
-    );
-    setFilteredQuestions(filteredQuestions);
-  }, [searchTerm, questions]); // Runs whenever searchTerm or questions change
-
-  if (loading) {
-    return <p>Loading questions...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+  const { questions, totalPages } = await getAllUserQuestions(currentPage, 10, tab, searchTerm);
+  const agencyList = await getAgencyList();
+  const agencyMap = await getDynamicAgencyMap();
 
   return (
     <div className="container max-w-screen-lg mx-auto justify-between px-6">
       <QuestionNavbar
-        unassignedCount={questions.filter(q => q.agency === null).length}
-        setSearchTerm={setSearchTerm}
+        unassignedCount={199}
+        currentTab={tab}
+        searchTerm={searchTerm}
       />
       <div className="pt-6">
-        <AdminQuestionBox questions={filteredQuestions} />
+        <AdminQuestionBox
+          questions={questions}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          agencyMap={agencyMap}
+          agencies={agencyList}
+        />
       </div>
     </div>
   );
