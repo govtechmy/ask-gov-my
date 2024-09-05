24df3e1999754c8a79c515189776c8c8ee763bf6
Wan Aqim <wanaqim@gmail.com>
2024-09-05 12:48:27 +0800

    remove translation from server component

--- frontend/app/[locale]/admin/dashboard/page.tsx
+++ frontend/app/[locale]/admin/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { useTranslations } from 'next-intl';
 import ManageQuestions from '@/components/AdminDashboard/ManageQuestions';
 import StaffHeaderDashboard from '@/components/common/Header/StaffHeaderDashboard';
 import StaffManageQuestions from '@/components/AdminDashboard/StaffManageQuestion';
@@ -8,7 +7,6 @@ interface DashboardPageProps {
 }
 
 export default function DashboardPage({ searchParams }: DashboardPageProps) {
-  const t = useTranslations('Adminlogin');
 
   let role = 'staff'; //user role will be extracted from user.role in real scenario
 
