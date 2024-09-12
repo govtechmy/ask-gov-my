f915fd8ad3d7fbb739a69b380f641ebbb16e6867
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2024-09-12 16:27:40 +0800

    fix: logic

--- frontend/app/api/auth/[...nextauth]/adapter.ts
+++ frontend/app/api/auth/[...nextauth]/adapter.ts
@@ -1,4 +1,5 @@
 import { get, post, put } from '@/lib/api';
+import { User } from 'next-auth';
 import type {
   Adapter,
   AdapterUser,
@@ -42,38 +43,10 @@ export const DjangoAdapter = (): Adapter => {
     },
 
     async getUserByAccount({ providerAccountId, provider }) {
-      try {
-        const response = await fetch(
-          `${API_URL}/auth/account?provider=${provider}&providerAccountId=${providerAccountId}`,
-          {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-            cache: 'no-store',
-          },
-        );
-        const data = await response.json();
-
-        if (response.ok && data.status === 'success') {
-          return {
-            id: data.data.id,
-            name: data.data.name,
-            email: data.data.email,
-            emailVerified: data.data.emailVerified,
-            role: data.data.role,
-            agency: data.data.agency,
-          };
-        } else if (response.status === 404) {
-          // In the case of account not found
-          return null;
-        } else {
-          // Handle other types of errors
-          console.error('Error fetching user account:', data.data.message);
-          return null;
-        }
-      } catch (error) {
-        console.error('Error in getUserByAccount:', error);
-        return null;
-      }
+      return await get<User | null>(`${API_URL}/auth/account`, {
+        provider,
+        providerAccountId,
+      });
     },
 
     async updateUser(user) {
