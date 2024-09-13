cf05fff3482e8f1a38920cc7525c5eb163ecd20e
ahmadanwar93 <ahmadfikri.anwarfadzil@gmail.com>
2024-09-13 10:44:42 +0800

    fix: remove-redirect-from-login

--- frontend/app/[locale]/admin/(auth)/page.tsx
+++ frontend/app/[locale]/admin/(auth)/page.tsx
@@ -2,16 +2,8 @@
 
 import { LoginForm } from '@/components/AdminLogin/LoginForm';
 import Footer from '@/components/common/Footer';
-import { useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
 
 export function AdminPage() {
-  const { data: session, status } = useSession();
-
-  if (session?.user) {
-    redirect('/admin/dashboard');
-  }
-
   return (
     <>
       <LoginForm />
