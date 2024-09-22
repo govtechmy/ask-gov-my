630583d17410963e091926563361adf8c42c5e70
Turbobot <turbobot@vercel.com>
2024-09-22 22:19:34 +0800

    feat(create-turbo): apply official-starter transform

--- backend/ask_gov/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AskGovConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ask_gov'
-
-    def ready(self):
-        import ask_gov.signals
