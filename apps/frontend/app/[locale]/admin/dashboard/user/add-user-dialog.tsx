ef1620ed846b2e74635a28616aabcf7115909af6
Daniel Zairin <danielzairink@gmail.com>
2024-10-21 17:13:44 +0800

    add user form

---
+++ apps/frontend/app/[locale]/admin/dashboard/user/add-user-dialog.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Translator from "@/components/client/translator";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+  DialogDescription,
+  PlusIcon,
+  DialogFooter,
+  DialogTitle,
+} from "@askgovmy/ui";
+import { UserForm, UserFormFields, UserFormSubmit } from "./user-form";
+import { Agency } from "@/types/types";
+import { useState } from "react";
+
+type AddUserDialogProps = {
+  agencies: Agency[];
+};
+
+export function AddUserDialog({ agencies }: AddUserDialogProps) {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={"primary"}
+          size={"sm"}
+          icon={<PlusIcon className="stroke-white-forcewhite" />}
+        >
+          <Translator
+            className="hidden md:block"
+            namespace="AdminUsers.add_new"
+          />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[600px] h-screen sm:h-fit">
+        <UserForm
+          className="flex flex-col"
+          onSubmit={(formValues) => {
+            window.alert(JSON.stringify({ formValues }, null, 2));
+            setOpen(false);
+          }}
+        >
+          <DialogHeader>
+            <DialogTitle>
+              <Translator namespace="UserForm.add_new" tag="span" />
+            </DialogTitle>
+            <DialogDescription className="sr-only">
+              <Translator namespace="UserForm.add_new" tag="span" />
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex-1 my-5">
+            <UserFormFields agencies={agencies} />
+          </div>
+          <DialogFooter className="flex flex-row gap-2 justify-end">
+            <Button type="button" onClick={() => setOpen(false)}>
+              <Translator namespace="UserForm.cancel" />
+            </Button>
+            <UserFormSubmit>
+              <Translator namespace="UserForm.save" />
+            </UserFormSubmit>
+          </DialogFooter>
+        </UserForm>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ apps/frontend/app/[locale]/admin/dashboard/user/user-form.tsx
@@ -0,0 +1,264 @@
+"use client";
+
+import {
+  Input,
+  FormLabel,
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormMessage,
+  FormSubmit,
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+  SelectIcon,
+  ChevronDownIcon,
+  Popover,
+  Button,
+  Command,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  PopoverRoot,
+  PopoverContent,
+  PopoverTrigger,
+} from "@askgovmy/ui";
+import { useForm, useFormContext } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { ComponentProps, PropsWithChildren, useState } from "react";
+import Translator from "@/components/client/translator";
+import { Agency } from "@/types/types";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  role: z.enum(["staff", "super_admin"]),
+  agency: z.number().nullable(),
+});
+type FormValues = z.infer<typeof formSchema>;
+export type UserFormValues = FormValues;
+
+interface UserFormProps extends PropsWithChildren {
+  className?: string;
+  onSubmit: (formValues: FormValues) => void | Promise<void>;
+  defaultValues?: FormValues;
+}
+
+/**
+ * @example
+ * ```tsx
+ * <UserForm onSubmit={fn}>
+ *   <h2>Create a user</h2>
+ *   <UserFormFields />
+ *   <UserFormSubmit>Save</UserFormSubmit>
+ * </UserForm>
+ * ```
+ */
+export function UserForm({
+  children,
+  className,
+  onSubmit,
+  defaultValues,
+}: UserFormProps) {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues || {
+      name: "",
+      email: "",
+      role: "staff",
+      agency: null,
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className={className} onSubmit={form.handleSubmit(onSubmit)}>
+        {children}
+      </form>
+    </Form>
+  );
+}
+
+export function useUserForm() {
+  const form = useFormContext<FormValues>();
+  if (!form) {
+    throw Error("Must use `useUserForm` within a `<UserForm />` component");
+  }
+  return form;
+}
+
+type UserFormFieldsProps = {
+  agencies: Pick<Agency, "id" | "name" | "acronym">[];
+};
+export function UserFormFields({ agencies }: UserFormFieldsProps) {
+  const form = useUserForm();
+  const [openAgencyPopover, setOpenAgencyPopover] = useState(false);
+  const [selectedAgency, setSelectedAgency] = useState<
+    UserFormFieldsProps["agencies"][number] | null
+  >(null);
+
+  return (
+    <div className="grid gap-4">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormLabel>
+              <Translator namespace="UserForm.name" />
+            </FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormLabel>
+              <Translator namespace="UserForm.email" />
+            </FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="role"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormLabel>
+              <Translator namespace="UserForm.role" />
+            </FormLabel>
+            <Select onValueChange={field.onChange} defaultValue={field.value}>
+              <FormControl>
+                <SelectTrigger className="flex items-center justify-between h-10 w-full bg-white shadow-button rounded-md border px-3 py-2 text-sm placeholder:text-muted-foreground // focus:border focus:border-askmygovbrand-300  focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 // focus:ring-0">
+                  <SelectValue />
+                  <SelectIcon>
+                    <ChevronDownIcon />
+                  </SelectIcon>
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                <SelectItem value="staff">Staff</SelectItem>
+                <SelectItem value="super_admin">Super Admin</SelectItem>
+              </SelectContent>
+            </Select>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      {form.watch("role") === "staff" && (
+        <FormField
+          control={form.control}
+          name="agency"
+          render={() => (
+            <FormItem className="w-full">
+              <FormLabel>
+                <Translator namespace="UserForm.agency" />
+              </FormLabel>
+              <PopoverRoot
+                modal
+                open={openAgencyPopover}
+                onOpenChange={setOpenAgencyPopover}
+              >
+                <FormControl>
+                  <PopoverTrigger className="flex items-center justify-between h-10 w-full bg-white shadow-button rounded-md border px-3 py-2 text-sm placeholder:text-muted-foreground // focus:border focus:border-askmygovbrand-300  focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 // focus:ring-0">
+                    {selectedAgency ? (
+                      <div className="flex gap-2 text-start">
+                        <span className="font-medium">
+                          {selectedAgency.acronym}
+                        </span>
+                        <span className="flex-1 w-full line-clamp-1 text-dim-500">
+                          {selectedAgency.name}
+                        </span>
+                      </div>
+                    ) : (
+                      <Translator
+                        namespace="UserForm.agency_unassigned"
+                        tag="span"
+                      />
+                    )}
+                    <SelectIcon>
+                      <ChevronDownIcon />
+                    </SelectIcon>
+                  </PopoverTrigger>
+                </FormControl>
+                <PopoverContent
+                  className="w-[--radix-popover-trigger-width] max-h-[--radix-popover-content-available-height]"
+                  align="start"
+                  side="bottom"
+                >
+                  <Command
+                    filter={(value, _search) => {
+                      const search = _search.toLowerCase();
+                      const item = agencies.find(
+                        (agency) => agency.id.toString() === value
+                      );
+                      return item?.name.toLowerCase().includes(search) ||
+                        item?.acronym.toLowerCase().includes(search)
+                        ? 1
+                        : 0;
+                    }}
+                  >
+                    <CommandInput placeholder="Search" />
+                    <CommandList>
+                      <CommandEmpty>
+                        <Translator namespace="AdminAgencies.not_found" />
+                      </CommandEmpty>
+                      <CommandGroup>
+                        {agencies.map((agency) => (
+                          <CommandItem
+                            key={agency.id}
+                            value={agency.id.toString()}
+                            onSelect={(agencyId) => {
+                              form.setValue(
+                                "agency",
+                                Number.parseInt(agencyId)
+                              );
+                              setSelectedAgency(agency);
+                              setOpenAgencyPopover(false);
+                            }}
+                            className="gap-2"
+                          >
+                            <span className="font-medium">
+                              {agency.acronym}
+                            </span>
+                            <span className="flex-1 w-full line-clamp-1 text-dim-500">
+                              {agency.name}
+                            </span>
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </PopoverRoot>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      )}
+    </div>
+  );
+}
+
+export function UserFormSubmit(
+  props: Omit<ComponentProps<typeof FormSubmit>, "form">
+) {
+  const form = useUserForm();
+  return <FormSubmit form={form} {...props} />;
+}
