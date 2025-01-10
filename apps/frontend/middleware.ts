86a7c992ee27d9347cf256c0cffe6733cae7aa92
Daniel Zairin <danielzairink@gmail.com>
2025-01-10 10:59:08 +0800

    feat: password protect staging deployment

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,13 +1,14 @@
 import { NextChain } from "./middlewares/chain";
 import { I18nMiddleware } from "./middlewares/i18n";
 import { RouterMiddleware } from "./middlewares/router";
+import { StagingAuthMiddleware } from "./middlewares/stagingAuth";
 
 /**
  * Note:
  * 1. Sequence of middleware matters!
  * 2. Final middleware should return NextResponse
  */
-export default NextChain([I18nMiddleware]);
+export default NextChain([StagingAuthMiddleware, I18nMiddleware]);
 
 export const config = {
   matcher: [
